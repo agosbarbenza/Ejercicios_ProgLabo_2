c9e2f413be7f2a38eaae040deed5d9fe5322e2df
Agostina Barbenza <agosbarbenza@gmail.com>
2023-04-09 16:34:35 -0300

    Add project files.

---
+++ Clases_Metodos_Estaticos/EJ_I02/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Text;
+namespace EJ_I02
+{
+    //Ejercicio I02 - ¿Desea continuar?
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            StringBuilder sb = new StringBuilder();
+        }
+    }
+}
---
+++ Intro/I_03/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+namespace I_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese un numero: ");
+           // int numeroIngresado = int.Parse(Console.ReadLine());
+           // bool sePudo = int.TryParse(numeroIngresado, out int resultado); 
+           //Console.WriteLine(ListarNumerosPrimos(numeroIngresado));
+           //Ya no usamos puntero, usamos ref o out                declaro la variable result
+            bool respuesta = int.TryParse(Console.ReadLine(), out int result);
+        }
+
+        public static string ListarNumerosPrimos(int hasta)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 2; i <= hasta; i++)
+            {
+                //Aca va la funcion que determina si un nuermo es primo
+                if (EsPrimo(i))
+                {
+                    sb.Append($"{i}, "); //El $ es interpolacion de strings, y codigo C# entre llaves.
+
+                }
+            }
+            return sb.ToString(); //Tengo que ponerle el toString porque estoy devolviendo un string
+        }
+
+        public static bool EsPrimo(int numero)
+        {
+            for (int i = 2; i < numero; i++)
+            {
+                if (numero % i == 0)
+                {
+                    return false;
+                }
+
+                
+            }
+            return true;
+        }
+    }
+}
